6ff170f8b4d1b9a6ee5ddec9c545d8d374699416
ko-ko-naing <kokonaingk723@gmail.com>
2024-09-28 16:09:24 +0630

    Data Type and Intro to Variable

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+//1 string data type
 console.log('hello world');
+
+//2 number data type
+console.log(12);
+console.log(12.15);
+
+//3 Boolean data type
+console.log(true);
+console.log(false);
+
+//4 nudefined 
+console.log(undefined);
+
+//5 null
+console.log(null);
+
+var a;
+console.log(a);
+var b = null;
+console.log(b);
+
+//variable
+
+var aungaungName = "Aung Aung";
+console.log(aungaungName);
+//changing value - override
+aungaungName = "changed aung aung";
+console .log(aungaungName);
