b1e9eeb322526cf2004953f13fc4a8dc558212eb
ko-ko-naing <kokonaingk723@gmail.com>
2024-09-28 16:47:18 +0630

    Variable Format and Es6 Variable Syntax

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 //1 string data type
 console.log('hello world');
 
@@ -26,4 +27,48 @@ var aungaungName = "Aung Aung";
 console.log(aungaungName);
 //changing value - override
 aungaungName = "changed aung aung";
-console .log(aungaungName);
+console.log(aungaungName);
+
+// Lesson 3 - variable naming convention
+var aungaung_name;
+var AngGyi;
+
+// JavaScript version
+var ES5;
+// ES5 version
+var number = 12;
+console.log(number);
+
+
+var ES6;
+// ES6 variable naming convention
+// let 
+let aungaungNo = "Aung Aung";
+console.log(aungaungNo);
+
+// difference between let and var  
+
+// const is constant - it's never changed or override 
+const kyawkyaw_name = "kyawkyaw";
+const PI = 3.14;
+
+
+// type of correction(+)
+var number1 = 12;
+var number2 = 13;
+console.log(number1 + number2);
+
+number1 = "12";
+number2 = 14;
+console.log(number1 + number2);
+
+let name = "ko ko naing";
+let age = 19;
+let isMarried = false;
+console.log(name + " age is " + age + " is Married: " + isMarried);
+ 
+//**** */
+let num1 = "12";
+let num2 = 14;
+//result is -2
+console.log(num1 - num2);
